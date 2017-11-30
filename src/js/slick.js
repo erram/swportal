6abdad25bedf47af50942f1d4bed7a1b4d3e441e
Alexandra Kiss <alexandra.kiss@hp.com>
2017-11-30 22:26:45 +0100

    add slick slider sources

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,22 +1,3 @@
-$(function(){
-  var navbar = $(".site-navigation nav");
-  var dropdowns = $(".site-navigation nav .dd");
-  $(".open-menu").click(function(e){
-    e.preventDefault();
-    navbar.toggleClass("open");
-  });
-
-  dropdowns.click(function(){
-    $(this).toggleClass("open");
-  });
-
-  $(window).resize(function() {
-    if(window.innerWidth > 1000) {
-      navbar.removeClass("open");
-      dropdowns.removeClass("open");
-    }
-  });
-});
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
