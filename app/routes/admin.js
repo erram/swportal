05934dcf68beb43da2110c05ac6993d04d40cd3d
Richard Molnar <Richard_Molnar@epam.com>
2017-11-30 14:48:12 +0100

    Added new list view

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,14 +1,19 @@
 var app = require("../../app")
 var isLoggedIn = require("../utils/auth")
-var News = require('../models/newsitem')
-var Card = require('../models/card')
+var News = require("../models/newsitem")
+var Card = require("../models/card")
 
-app.get("/admin/news", isLoggedIn, function (req, res) {
-    News.find({ Publikálva: false }, function (err, news) {
-        if (err) {
-            res.send(err)
-        } else {
-            res.render("admin/news.ejs", { news: news })
-        }
-    })
+app.get("/admin/news", isLoggedIn, function(req, res) {
+  var moment = require("moment")
+  News.find({ Publikálva: false }, function(err, news) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.render("admin/news.ejs", {
+        news: news,
+        usr: req.user.local.email,
+        moment: moment
+      })
+    }
+  })
 })
