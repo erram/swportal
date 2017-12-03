1e3fdee7cfd45b4263951e6c87259b28d6fa0a42
richard molnar <erramgeor@gmail.com>
2017-12-03 21:26:05 +0100

    status update

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,7 +5,7 @@ var Card = require("../models/card")
 
 app.get("/admin/news", isLoggedIn, function(req, res) {
   var moment = require("moment")
-  News.find(/*{ Publikálva: false },*/{}, function(err, news) {
+  News.find({}, function(err, news) {
     if (err) {
       res.send(err)
     } else {
@@ -14,6 +14,24 @@ app.get("/admin/news", isLoggedIn, function(req, res) {
         usr: req.user.local.email,
         moment: moment
       })
+    }
+  })
+})
+
+app.post("/admin/news/updatestatus", function (req, res) {
+
+  News.findOne({ "ID": req.body.newsID }, function (err, newsitem) {
+    newsitem.Publikálva = req.body.status
+    if (newsitem) {
+      newsitem.save(function (err) {
+        if (err)
+          console.log(newsitem._id + ' failed!')
+        else
+          console.log(newsitem._id + ' updated!')
+      })
+    } else {
+      console.log(err)
+      res.send(err)
     }
   })
 })
