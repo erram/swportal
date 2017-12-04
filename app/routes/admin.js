a2801ef64b7014b4581ee5cb3c4bb965cec65f22
richard molnar <erramgeor@gmail.com>
2017-12-04 23:25:21 +0100

    User role admin manage

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,6 +3,7 @@ var isLoggedIn = require("../utils/auth")
 var isAdmin = require("../utils/isadmin")
 var News = require("../models/newsitem")
 var Card = require("../models/card")
+var User = require("../models/user")
 
 app.get("/admin/news", isLoggedIn, isAdmin, function(req, res) {
   var moment = require("moment")
@@ -19,8 +20,20 @@ app.get("/admin/news", isLoggedIn, isAdmin, function(req, res) {
   })
 })
 
-app.post("/admin/news/updatestatus", function (req, res) {
+app.get("/admin/users", isLoggedIn, isAdmin, function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) {
+      res.send(err)
+    } else {
+      res.render("admin/users.ejs", {
+        users: users
+      })
+    }
+  })
+})
 
+
+app.post("/admin/news/updatestatus", function (req, res) {
   News.findOne({ "ID": req.body.newsID }, function (err, newsitem) {
     newsitem.Publikálva = req.body.status
     if (newsitem) {
@@ -37,3 +50,22 @@ app.post("/admin/news/updatestatus", function (req, res) {
     }
   })
 })
+
+app.post("/admin/user/updatestatus", function (req, res) { 
+    User.findOne({ "local.email": req.body.userEmail }, function (err, user) {
+      user.local.role = req.body.status
+      if (user) {
+        user.save(function (err) {
+          if (err)
+            console.log(user.local.email + ' failed!')
+          else
+            return res.status(200).send("OK")
+            console.log(user.local.email + ' updated!')
+        })
+      } else {
+        console.log(err)
+        res.send(err)
+      }
+    })
+  })
+  
