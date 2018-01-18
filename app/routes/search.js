15d469bce9e16b1d1108214034ba25b57fb9dcb8
Richard Molnar <Richard_Molnar@epam.com>
2018-01-18 12:45:04 +0100

    Search bar

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,7 +1,21 @@
 var app = require("../../app")
+var Card = require('../models/card')
 
 
 //Minden kártya kiirása
 app.get("/search", function(req, res) {
     res.render("search.ejs")
+})
+
+app.post("/search/autocomplete", function(req, res) {
+    console.log("hello")
+    var keyword = req.body.keyword
+    Card.find({"Név":new RegExp(keyword, 'i')},'Név',function(err, itms) {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(JSON.stringify(itms))
+        }
+      })
+	
 })
