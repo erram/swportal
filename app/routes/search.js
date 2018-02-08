9103ae620d96ca60c3671802d53b2631ea8ddaf0
Richard Molnar <Richard_Molnar@epam.com>
2018-02-08 11:41:13 +0100

    Advanced search

--- app/routes/search.js
+++ app/routes/search.js
@@ -40,36 +40,109 @@ app.get("/card/single/:kiadas/:sorszam", function(req, res) {
 
 app.post("/search/advanced", function(req, res) {
   if (req.body) {
-    //console.log("fasz")
-    //console.log(req.body)
     var queryObj = {}
     var data = req.body
     var querryarray = []
 
-    for (var key in data){
-        var attrName = key;
-        var attrValue = data[key];
-        //console.log("key:"+attrName+", data:"+attrValue)
+    console.log(data)
+    
+    if(data.name) {
+      querryarray.push({'Név':new RegExp(data.field, "i")})
+    }
+    
+    if(data.text) {
+      querryarray.push({'Szövegmező':new RegExp(data.field, "i")})
+    }
+
+    if(data.illustrator) {
+      querryarray.push({'Illusztrátor':new RegExp(data.field, "i")})
+    }
+
+    if(data.quote) {
+      querryarray.push({'Idézet':new RegExp(data.field, "i")})
     }
 
     if(data.subtype) {
-        querryarray.push({'Típus':new RegExp(data.field, "i")})
+      querryarray.push({'Típus':data.subtype})
+    }
+
+    if(data.set) {
+      querryarray.push({'Kiadás':data.set})
+    }
+        
+
+    if(data.color) {
+      var temp = {}
+      var array = [{}]
+      if(Array.isArray(data.color)) {
+        array = data.color.map(function(item){
+          return {"Frakció":item}
+        })
+        temp.$or = array;
+      } else {
+        temp.$or = [{"Frakció":data.color}]
+      }
+      
+      querryarray.push(temp);
+    }
+
+    if(data.side) {
+      var temp = {}
+      var array = [{}]
+      if(Array.isArray(data.side)) {
+        array = data.side.map(function(item){
+          return {"Oldal":item}
+        })
+        temp.$or = array;
+      } else {
+        temp.$or = [{"Oldal":data.side}]
+      }
+      
+      querryarray.push(temp);
+    }
+
+    if(data.type) {
+      var temp = {}
+      var array = [{}]
+      if(Array.isArray(data.type)) {
+        array = data.type.map(function(item){
+          return {"Típus":item}
+        })
+        temp.$or = array;
+      } else {
+        temp.$or = [{"Típus":data.type}]
+      }
+      
+      querryarray.push(temp);
+    }
+
+    if(data.rarity) {
+      var temp = {}
+      var array = [{}]
+      if(Array.isArray(data.rarity)) {
+        array = data.rarity.map(function(item){
+          return {"Ritkaság":item}
+        })
+        temp.$or = array;
+      } else {
+        temp.$or = [{"Ritkaság":data.rarity}]
+      }
+      
+      querryarray.push(temp);
     }
 
     queryObj.$and = querryarray
 
-    /*
-    .find( {
-    $and : [
-        {'Oldal':'Hős'},
-        {$or:[ {'Frakció': 'Kék'}, {'Frakció': 'Sárga'} ]}
-    ] 
-    })
-    */
-
     console.log(queryObj)
+    Card.find(queryObj,
+      function(err, itms) {
+        if (err) {
+          res.status(500).send(err)
+        } else {
+          res.render("cardslist.ejs",{ cards: itms, user: req.user })
+        }
+      })
   } else {
-    console.log("pina")
+    res.status(500).send(res)
   }
-  res.status(200).redirect("/search")
 })
